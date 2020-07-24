62280013c2f5b98c562b92c27af94916ba296bbb
guilherme-carra <gui_gc@hotmail.com>
2020-07-24 18:07:33 +0200

    page basic structure done

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <!-- TODO Application entry point. Login view -->
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <body>
+        <input type="email" id="email" placeholder="Insert an Email"/>
+
+        <input  type="password" id="password" placeholder="Insert a Password"/>
+
+        <button id="loginBtn">Log in</button>
+
+        <script src="node_modules/jquery/dist/jquery.min.js"></script>
+        <script src="src/js/script.js"></script>
+    </body>
+</html>
