06ec8fd209b7d8c75a40eafad6cef84b9c6fba24
guilherme-carra <gui_gc@hotmail.com>
2020-07-24 18:07:47 +0200

    finished basic page structure

--- src/dashboard.php
+++ src/dashboard.php
@@ -1,1 +1,23 @@
-<!-- TODO Main view or Employees Grid View here is where you get when logged here there's the grid of employees -->
+<!-- TODO Application entry point. Login view -->
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <body>
+        <form method="POST" action="library/loginController.php">
+            <input type="submit" name="logout" value="Log Out"/>
+        </form>
+
+        <div id="jsGrid"></div>
+
+        <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+        <script src="../node_modules/jsgrid/dist/jsgrid.min.js"></script>
+        <script src="../src/js/script.js"></script>
+    </body>
+</html>
