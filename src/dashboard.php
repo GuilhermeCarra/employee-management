92d889f121b33db36314d158113e8a820c18ea53
guilherme-carra <gui_gc@hotmail.com>
2020-07-27 14:54:17 +0200

    changed employee tab form to employee.php

--- src/dashboard.php
+++ src/dashboard.php
@@ -19,8 +19,8 @@
 
             <!-- DASHBOARD / EMPLOYEE NAVIGATION -->
             <ul>
-                <li>Dashboard</li>
-                <li>Employee</li>
+                <li id="navDashboard">Dashboard</li>
+                <li id="navEmployee">Employee</li>
             </ul>
 
             <!-- LOGOUT BUTTON -->
@@ -29,62 +29,7 @@
             </form>
         </header>
 
-        <div>
-            <!-- DASHBOARD TAB -->
-            <div id="jsGrid"></div>
-
-            <!-- EMPLOYEE TAB -->
-            <div id="employeeTab">
-                <form>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                        <label for="">Name</label>
-                        <input type="text" class="form-control" name="name">
-                        </div>
-                        <div class="form-group col-md-6">
-                        <label for="">Last Name</label>
-                        <input type="text" class="form-control" name="lastName">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="">Email address</label>
-                        <input type="text" class="form-control" id="" placeholder="">
-                    </div>
-                    <div class="form-group">
-                        <label for="">Address 2</label>
-                        <input type="text" class="form-control" id="" placeholder="Apartment, studio, or floor">
-                    </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                        <label for="">City</label>
-                        <input type="text" class="form-control" id="">
-                        </div>
-                        <div class="form-group col-md-4">
-                        <label for="">Gender</label>
-                        <select id="" class="form-control">
-                            <option selected>Choose...</option>
-                            <option>Man</option>
-                            <option>Woman</option>
-                        </select>
-                        </div>
-                        <div class="form-group col-md-2">
-                        <label for="">Zip</label>
-                        <input type="text" class="form-control" id="">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="">
-                        <label class="form-check-label" for="">
-                            Check me out
-                        </label>
-                        </div>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Sign in</button>
-                </form>
-            </div>
-
-        </div>
+        <div id="jsGrid"></div>
 
         <script src="../node_modules/jquery/dist/jquery.min.js"></script>
         <script src="../node_modules/jsgrid/dist/jsgrid.min.js"></script>
--- src/employee.php
+++ src/employee.php
@@ -1,1 +1,99 @@
-<!-- TODO Employee view -->
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../node_modules/jsgrid/css/jsgrid.css">
+    <link rel="stylesheet" href="../node_modules/jsgrid/css/theme.css">
+</head>
+    <body>
+        <header>
+            <!-- LOGO -->
+            <h4>LOGO</h4>
+
+            <!-- DASHBOARD / EMPLOYEE NAVIGATION -->
+            <ul>
+                <li id="navDashboard">Dashboard</li>
+                <li id="navEmployee">Employee</li>
+            </ul>
+
+            <!-- LOGOUT BUTTON -->
+            <form method="POST" action="library/loginController.php">
+                <input type="submit" name="logout" value="Log Out"/>
+            </form>
+        </header>
+
+        <form>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="">Name</label>
+                    <input type="text" class="form-control" name="name">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="">Last Name</label>
+                    <input type="text" class="form-control" name="lastName">
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="email">Email address</label>
+                    <input type="text" class="form-control" name="email" id="email" placeholder="">
+                    <small>We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="gender">Gender</label>
+                    <select id="gender" name="gender" class="form-control">
+                        <option>Man</option>
+                        <option>Woman</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="city">City</label>
+                    <input type="text" class="form-control" name="city" id="city" placeholder="">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="streetAddress">Street Address</label>
+                    <input type="text" class="form-control" name="streetAddress" id="streetAddress" placeholder="">
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="state">State</label>
+                    <input type="text" class="form-control" name="state" id="state" placeholder="">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="age">Age</label>
+                    <input type="number" class="form-control" name="age" id="age" placeholder="">
+                </div>
+            </div>
+
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="postalCode">Postal Code</label>
+                    <input type="number" class="form-control" name="postalCode" id="postalCode" placeholder="">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="phoneNumber">Phone Number</label>
+                    <input type="number" class="form-control" name="phoneNumber" id="phoneNumber" placeholder="">
+                </div>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Save</button>
+            <button class="btn btn-secondary">Return</button>
+        </form>
+
+        <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+        <script src="../node_modules/jsgrid/dist/jsgrid.min.js"></script>
+        <script src="../src/js/script.js"></script>
+    </body>
+</html>
