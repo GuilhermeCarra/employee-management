1a0bef570586da802d238e88dda2fa6bb5c7396d
guilherme-carra <gui_gc@hotmail.com>
2020-07-27 18:57:46 +0200

    changed iputs fill to be made by script.js

--- src/employee.php
+++ src/employee.php
@@ -1,18 +1,6 @@
 <?php
-    $method = "POST";
-    if( isset($_GET['id']) ) {
-        $method = "PUT";
-        $url = $_SERVER['SERVER_NAME'].dirname($_SERVER['REQUEST_URI']).'/library/employeeController.php?id='.$_GET['id'];
-        $handle = curl_init($url);
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-        $employee = json_decode(curl_exec($handle));
-        curl_close($handle);
-        if ($employee->gender == "man") {
-            $male = true;
-        } else {
-            $female = true;
-        }
-    }
+    $method = 'POST';
+    if ( isset($_GET['id']) ) $method = 'PUT';
 ?>
 
 <!DOCTYPE html>
@@ -45,26 +33,26 @@
         <div>
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <label for="">Name</label>
-                    <input type="text" class="form-control" name="name" value="<?php if(isset($employee)) echo $employee->name?>">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name">
                 </div>
                 <div class="form-group col-md-6">
-                    <label for="">Last Name</label>
-                    <input type="text" class="form-control" name="lastName" value="<?php if(isset($employee)) echo $employee->lastName?>">
+                    <label for="lastName">Last Name</label>
+                    <input type="text" class="form-control" id="lastName">
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="email">Email address</label>
-                    <input type="text" class="form-control" name="email" id="email" placeholder="" value="<?php if(isset($employee)) echo $employee->email?>">
+                    <input type="text" class="form-control" id="email" placeholder="">
                     <small>We'll never share your email with anyone else.</small>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="gender">Gender</label>
-                    <select id="gender" name="gender" class="form-control">
-                        <option <?php if(isset($male)) echo 'selected' ?> >Man</option>
-                        <option <?php if(isset($female)) echo 'selected' ?> >Woman</option>
+                    <select id="gender" class="form-control">
+                        <option value="man">Man</option>
+                        <option value="woman">Woman</option>
                     </select>
                 </div>
             </div>
@@ -72,38 +60,38 @@
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="city">City</label>
-                    <input type="text" class="form-control" name="city" id="city" placeholder="" value="<?php if(isset($employee)) echo $employee->city?>">
+                    <input type="text" class="form-control" id="city" placeholder="">
                 </div>
                 <div class="form-group col-md-6">
                     <label for="streetAddress">Street Address</label>
-                    <input type="text" class="form-control" name="streetAddress" id="streetAddress" placeholder="" value="<?php if(isset($employee)) echo $employee->streetAddress?>">
+                    <input type="text" class="form-control" id="streetAddress" placeholder="">
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="state">State</label>
-                    <input type="text" class="form-control" name="state" id="state" placeholder="" value="<?php if(isset($employee)) echo $employee->state?>">
+                    <input type="text" class="form-control" id="state" placeholder="">
                 </div>
                 <div class="form-group col-md-6">
                     <label for="age">Age</label>
-                    <input type="number" class="form-control" name="age" id="age" placeholder="" value="<?php if(isset($employee)) echo $employee->age?>">
+                    <input type="number" class="form-control" id="age" placeholder="">
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="postalCode">Postal Code</label>
-                    <input type="number" class="form-control" name="postalCode" id="postalCode" placeholder="" value="<?php if(isset($employee)) echo $employee->postalCode?>">
+                    <input type="number" class="form-control" id="postalCode" placeholder="">
                 </div>
                 <div class="form-group col-md-6">
                     <label for="phoneNumber">Phone Number</label>
-                    <input type="number" class="form-control" name="phoneNumber" id="phoneNumber" placeholder="" value="<?php if(isset($employee)) echo $employee->phoneNumber?>">
+                    <input type="number" class="form-control" id="phoneNumber" placeholder="">
                 </div>
             </div>
 
             <button id="employee<?php echo $method ?>" class="btn btn-primary">Save</button>
-            <button id="employeeReturn"class="btn btn-secondary">Return</button>
+            <a href="dashboard.php"><button id="employeeReturn"class="btn btn-secondary">Return</button></a>
         </div>
 
         <script src="../node_modules/jquery/dist/jquery.min.js"></script>
