1b7c0a4416606e4ef60aac958eb6f1f07eaa4ffc
guilherme-carra <gui_gc@hotmail.com>
2020-07-27 13:25:50 +0200

    added bootstrap

--- src/dashboard.php
+++ src/dashboard.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../node_modules/jsgrid/css/jsgrid.css">
     <link rel="stylesheet" href="../node_modules/jsgrid/css/theme.css">
 </head>
