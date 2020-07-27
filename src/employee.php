29871d2beda9a96f4dc9a082e28581bf684d7ad4
guilherme-carra <gui_gc@hotmail.com>
2020-07-27 17:01:16 +0200

    added php to fill inputs with employee id

--- src/employee.php
+++ src/employee.php
@@ -1,4 +1,18 @@
 <?php
+    $method = "POST";
+    if( isset($_GET['id']) ) {
+        $method = "PUT";
+        $url = $_SERVER['SERVER_NAME'].dirname($_SERVER['REQUEST_URI']).'/library/employeeController.php?id='.$_GET['id'];
+        $handle = curl_init($url);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        $employee = json_decode(curl_exec($handle));
+        curl_close($handle);
+        if ($employee->gender == "man") {
+            $male = true;
+        } else {
+            $female = true;
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -28,29 +42,29 @@
             </form>
         </header>
 
-        <form>
+        <form method="<?php echo $method ?>" action="employeeController.php">
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="">Name</label>
-                    <input type="text" class="form-control" name="name">
+                    <input type="text" class="form-control" name="name" value="<?php if(isset($employee)) echo $employee->name?>">
                 </div>
                 <div class="form-group col-md-6">
                     <label for="">Last Name</label>
-                    <input type="text" class="form-control" name="lastName">
+                    <input type="text" class="form-control" name="lastName" value="<?php if(isset($employee)) echo $employee->lastName?>">
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="email">Email address</label>
-                    <input type="text" class="form-control" name="email" id="email" placeholder="">
+                    <input type="text" class="form-control" name="email" id="email" placeholder="" value="<?php if(isset($employee)) echo $employee->email?>">
                     <small>We'll never share your email with anyone else.</small>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="gender">Gender</label>
                     <select id="gender" name="gender" class="form-control">
-                        <option>Man</option>
-                        <option>Woman</option>
+                        <option <?php if(isset($male)) echo 'selected' ?> >Man</option>
+                        <option <?php if(isset($female)) echo 'selected' ?> >Woman</option>
                     </select>
                 </div>
             </div>
@@ -58,33 +72,33 @@
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="city">City</label>
-                    <input type="text" class="form-control" name="city" id="city" placeholder="">
+                    <input type="text" class="form-control" name="city" id="city" placeholder="" value="<?php if(isset($employee)) echo $employee->city?>">
                 </div>
                 <div class="form-group col-md-6">
                     <label for="streetAddress">Street Address</label>
-                    <input type="text" class="form-control" name="streetAddress" id="streetAddress" placeholder="">
+                    <input type="text" class="form-control" name="streetAddress" id="streetAddress" placeholder="" value="<?php if(isset($employee)) echo $employee->streetAddress?>">
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="state">State</label>
-                    <input type="text" class="form-control" name="state" id="state" placeholder="">
+                    <input type="text" class="form-control" name="state" id="state" placeholder="" value="<?php if(isset($employee)) echo $employee->state?>">
                 </div>
                 <div class="form-group col-md-6">
                     <label for="age">Age</label>
-                    <input type="number" class="form-control" name="age" id="age" placeholder="">
+                    <input type="number" class="form-control" name="age" id="age" placeholder="" value="<?php if(isset($employee)) echo $employee->age?>">
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="postalCode">Postal Code</label>
-                    <input type="number" class="form-control" name="postalCode" id="postalCode" placeholder="">
+                    <input type="number" class="form-control" name="postalCode" id="postalCode" placeholder="" value="<?php if(isset($employee)) echo $employee->postalCode?>">
                 </div>
                 <div class="form-group col-md-6">
                     <label for="phoneNumber">Phone Number</label>
-                    <input type="number" class="form-control" name="phoneNumber" id="phoneNumber" placeholder="">
+                    <input type="number" class="form-control" name="phoneNumber" id="phoneNumber" placeholder="" value="<?php if(isset($employee)) echo $employee->phoneNumber?>">
                 </div>
             </div>
 
