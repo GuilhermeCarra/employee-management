7f8fd9f2d88c1302741a8ce3b6b295ad07fd4cf2
guilherme-carra <gui_gc@hotmail.com>
2020-07-27 13:27:46 +0200

    added function calls on controller

--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -2,14 +2,18 @@
 require('employeeManager.php');
 
 switch($_SERVER["REQUEST_METHOD"]) {
+
     case "GET":
         echo getAllEmployees();
         break;
+
     case "POST":
-        echo addEmployee();
+        echo addEmployee($_POST["newEmployee"]);
         break;
+
     case "DELETE":
-        echo deleteEmployee();
+        parse_str(file_get_contents("php://input"), $_DELETE);
+        echo deleteEmployee($_DELETE['id']);
         break;
 }
 ?>
