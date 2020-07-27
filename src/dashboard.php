59d6f1ca87f04487b5c3a71010780d17af399896
guilherme-carra <gui_gc@hotmail.com>
2020-07-27 13:26:21 +0200

    implementeed employee form

--- src/dashboard.php
+++ src/dashboard.php
@@ -13,11 +13,78 @@
     <link rel="stylesheet" href="../node_modules/jsgrid/css/theme.css">
 </head>
     <body>
-        <form method="POST" action="library/loginController.php">
-            <input type="submit" name="logout" value="Log Out"/>
-        </form>
+        <header>
+            <!-- LOGO -->
+            <h4>LOGO</h4>
 
-        <div id="jsGrid"></div>
+            <!-- DASHBOARD / EMPLOYEE NAVIGATION -->
+            <ul>
+                <li>Dashboard</li>
+                <li>Employee</li>
+            </ul>
+
+            <!-- LOGOUT BUTTON -->
+            <form method="POST" action="library/loginController.php">
+                <input type="submit" name="logout" value="Log Out"/>
+            </form>
+        </header>
+
+        <div>
+            <!-- DASHBOARD TAB -->
+            <div id="jsGrid"></div>
+
+            <!-- EMPLOYEE TAB -->
+            <div id="employeeTab">
+                <form>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                        <label for="">Name</label>
+                        <input type="text" class="form-control" name="name">
+                        </div>
+                        <div class="form-group col-md-6">
+                        <label for="">Last Name</label>
+                        <input type="text" class="form-control" name="lastName">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Email address</label>
+                        <input type="text" class="form-control" id="" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Address 2</label>
+                        <input type="text" class="form-control" id="" placeholder="Apartment, studio, or floor">
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                        <label for="">City</label>
+                        <input type="text" class="form-control" id="">
+                        </div>
+                        <div class="form-group col-md-4">
+                        <label for="">Gender</label>
+                        <select id="" class="form-control">
+                            <option selected>Choose...</option>
+                            <option>Man</option>
+                            <option>Woman</option>
+                        </select>
+                        </div>
+                        <div class="form-group col-md-2">
+                        <label for="">Zip</label>
+                        <input type="text" class="form-control" id="">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="">
+                        <label class="form-check-label" for="">
+                            Check me out
+                        </label>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </form>
+            </div>
+
+        </div>
 
         <script src="../node_modules/jquery/dist/jquery.min.js"></script>
         <script src="../node_modules/jsgrid/dist/jsgrid.min.js"></script>
