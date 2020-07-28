34193737fff4bcc63759467009a4256762d95c31
Sandra Mitjans <mitjanssandra@gmail.com>
2020-07-28 14:26:13 +0200

    Employee CSS

--- src/employee.php
+++ src/employee.php
@@ -1,10 +1,11 @@
 <?php
-    $method = 'POST';
-    if ( isset($_GET['id']) ) $method = 'PUT';
+$method = 'POST';
+if (isset($_GET['id'])) $method = 'PUT';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,91 +13,90 @@
     <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
 </head>
-    <body>
-        <header>
-            <!-- LOGO -->
-            <h4>LOGO</h4>
 
-            <!-- DASHBOARD / EMPLOYEE NAVIGATION -->
-            <ul>
-                <a href="dashboard.php"><li>Dashboard</li></a>
-                <a href="employee.php"><li>Employee</li></a>
-            </ul>
+<body>
+    <?php require '../assets/header.html'; ?>
 
-            <!-- LOGOUT BUTTON -->
-            <form method="POST" action="library/loginController.php">
-                <input type="submit" name="logout" value="Log Out"/>
-            </form>
-        </header>
+    <div class="row">
+        <div class="col-1"></div>
+        <div class="col-10 mb-5">
+            <?php include('imageGallery.php') ?>
+            <div class="col-1"></div>
+            <div class="row form-employee">
+                <div class="col-2"></div>
+                <div class="col-8">
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" id="name">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="lastName">Last Name</label>
+                            <input type="text" class="form-control" id="lastName">
+                        </div>
+                    </div>
 
-        <div>
-            <?php include('imageGallery.php')?>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="name">Name</label>
-                    <input type="text" class="form-control" id="name">
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="email">Email address</label>
+                            <input type="text" class="form-control" id="email" placeholder="">
+                            <small>We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="gender">Gender</label>
+                            <select id="gender" class="form-control">
+                                <option value="man">Man</option>
+                                <option value="woman">Woman</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="city">City</label>
+                            <input type="text" class="form-control" id="city" placeholder="">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="streetAddress">Street Address</label>
+                            <input type="text" class="form-control" id="streetAddress" placeholder="">
+                        </div>
+                    </div>
+
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="state">State</label>
+                            <input type="text" class="form-control" id="state" placeholder="">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="age">Age</label>
+                            <input type="number" class="form-control" id="age" placeholder="">
+                        </div>
+                    </div>
+
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="postalCode">Postal Code</label>
+                            <input type="number" class="form-control" id="postalCode" placeholder="">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="phoneNumber">Phone Number</label>
+                            <input type="number" class="form-control" id="phoneNumber" placeholder="">
+                        </div>
+                    </div>
+                    <div class="buttons-employeed">
+                        <button id="employee<?php echo $method ?>" class="btn btn-outline-dark">Save</button>
+                        <a href="dashboard.php"><button id="employeeReturn" class="btn btn-dark ">Return</button></a>
+                    </div>
                 </div>
-                <div class="form-group col-md-6">
-                    <label for="lastName">Last Name</label>
-                    <input type="text" class="form-control" id="lastName">
-                </div>
+                <div class="col-2"></div>
             </div>
+            <div id="employeeAlert"></div>
 
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="email">Email address</label>
-                    <input type="text" class="form-control" id="email" placeholder="">
-                    <small>We'll never share your email with anyone else.</small>
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="gender">Gender</label>
-                    <select id="gender" class="form-control">
-                        <option value="man">Man</option>
-                        <option value="woman">Woman</option>
-                    </select>
-                </div>
-            </div>
+            <?php require '../assets/footer.html'; ?>
 
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="city">City</label>
-                    <input type="text" class="form-control" id="city" placeholder="">
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="streetAddress">Street Address</label>
-                    <input type="text" class="form-control" id="streetAddress" placeholder="">
-                </div>
-            </div>
+            <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+            <script src="../node_modules/jsgrid/dist/jsgrid.min.js"></script>
+            <script src="../src/js/script.js"></script>
+</body>
 
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="state">State</label>
-                    <input type="text" class="form-control" id="state" placeholder="">
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="age">Age</label>
-                    <input type="number" class="form-control" id="age" placeholder="">
-                </div>
-            </div>
-
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="postalCode">Postal Code</label>
-                    <input type="number" class="form-control" id="postalCode" placeholder="">
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="phoneNumber">Phone Number</label>
-                    <input type="number" class="form-control" id="phoneNumber" placeholder="">
-                </div>
-            </div>
-
-            <button id="employee<?php echo $method ?>" class="btn btn-primary">Save</button>
-            <a href="dashboard.php"><button id="employeeReturn"class="btn btn-secondary">Return</button></a>
-        </div>
-        <div id="employeeAlert"></div>
-
-        <script src="../node_modules/jquery/dist/jquery.min.js"></script>
-        <script src="../node_modules/jsgrid/dist/jsgrid.min.js"></script>
-        <script src="../src/js/script.js"></script>
-    </body>
 </html>
