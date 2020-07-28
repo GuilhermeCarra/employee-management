65d7a9df9ab06094dbd6be98b87a03a7319ee185
guilherme-carra <gui_gc@hotmail.com>
2020-07-28 16:36:37 +0200

    API implimented

--- src/library/avatarsApi.php
+++ src/library/avatarsApi.php
@@ -1,7 +1,21 @@
 <?php
 
 function getAvatars() {
-    $avatarsResponse = json_decode(file_get_contents("../resources/images_mock.json"));
+    //$avatarsResponse = json_decode(file_get_contents("../resources/images_mock.json"));
+    $key = '6301675F-934441A8-BFE795BC-BFC60F6B';
+
+    $ch = curl_init();
+    curl_setopt_array($ch, array(
+        CURLOPT_URL => "https://uifaces.co/api",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POSTFIELDS => "limit=10",
+        CURLOPT_HTTPHEADER => array('X-API-KEY: 6301675F-934441A8-BFE795BC-BFC60F6B')
+    ));
+    $output = curl_exec($ch);
+    curl_close($ch);
+
+    $avatarsResponse = json_decode($output);
+
     foreach($avatarsResponse as $avatar) {
         $avatars[] = $avatar->photo;
     }
