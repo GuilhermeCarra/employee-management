f4b04a896e88b22744652674e2e7969d99a4b358
Christian Callau <ch.callau@gmail.com>
2020-08-13 22:04:26 +0200

    Index controller

--- index.php
+++ index.php
@@ -1,54 +1,21 @@
 <?php
+
+include_once "config/constants.php";
+
 session_start();
-if (isset($_SESSION['name'])) {
-    header('Location: src/dashboard.php');
+
+if (isset($_GET["controller"])) {
+    $controller = CONTROLLERS . $_GET["controller"] . "Controller.php";
+
+    if (file_exists($controller)) include_once $controller;
+    else {
+        $errorMsg = "The requested controller does not exist";
+        include_once VIEWS . "error/error.php";
+    }
+} else {
+    if (isset($_SESSION['name'])) {
+        include_once VIEWS . "dashboard.php";
+    } else {
+        include_once VIEWS . "login.php";
+    }
 }
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/login.css">
-    <title>Employee Management</title>
-    <link rel="icon" type="image/png" href="assets/img/favicon.png">
-</head>
-
-<body id="loginPage">
-    <div class="form_bg">
-        <div class="container">
-            <div class="row">
-                <div class="col-4"></div>
-                <div class="col-4">
-                    <div class="form-horizontal">
-                        <div class="form_icon"><i class="fa fa-user-circle"></i></div>
-                        <h3 class="title">admin login</h3>
-                        <div class="form-group">
-                            <span class="input-icon"><i class="fa fa-user"></i></span>
-                            <input class="form-control" type="email" id="email" placeholder="Insert an Email" />
-                        </div>
-                        <div class="form-group">
-                            <span class="input-icon"><i class="fa fa-lock"></i></span>
-                            <input class="form-control" type="password" id="password" placeholder="Insert a Password" />
-                        </div>
-
-                        <button class="loginBtn" id="loginBtn">Log in</button>
-
-                    </div>
-                </div>
-                <div class="col-4"></div>
-            </div>
-        </div>
-    </div>
-    <div id="loginError"></div>
-
-    <script src="node_modules/jquery/dist/jquery.min.js"></script>
-    <script src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
-    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="assets/js/script.js"></script>
-</body>
-
-</html>
