b8a3ec1e78cd3b1fc93725e431f060da2e9d3b11
guilherme-carra <gui_gc@hotmail.com>
2020-09-03 16:15:31 +0200

    eliminated testing echo

--- libs/sessionHelper.php
+++ libs/sessionHelper.php
@@ -1,5 +1,4 @@
 <?php
-echo time() - $_SESSION['logTime'];
 if (isset($_SESSION['logged'])) {
    if (((time() - $_SESSION['logTime']) > 600) || isset($_POST['logout'])) {
       header("Location: ". BASE_URL ."/loginController/logout/");
